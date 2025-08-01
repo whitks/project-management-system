41347bde81f89e14b04c70fd343c5242c9831298
whitks <krishsharma1187@gmail.com>
2025-08-01 07:20:35 +0530

    repair

--- src/pages/components/SelectTeam.js
+++ src/pages/components/SelectTeam.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { Search } from 'lucide-react'
+import { Search, Plus, X } from 'lucide-react'
 import "./styles/selectteam.css"
 function SelectTeam() {
   const [team, setTeam] = useState([]);
   const [search, setSearch] = useState('');
   const [people, setPeople] = useState([]);
-  const [showModal, setShowModal] = useState(false)
+  const [showModal, setShowModal] = useState(false);
   function handleChange(e) {
     setSearch(e.target.value);
   }
@@ -14,6 +14,14 @@ function SelectTeam() {
   }
   function handleClose() {
     setShowModal(false)
+  }
+  function handleAddMember(e, k) {
+    setTeam(prev => {
+      const member = people[k];
+      return [...prev, member];
+    });
+    e.currentTarget.disabled = true;
+    e.currentTarget.style.cursor = "default";
   }
   useEffect(() => {
     const fetchData = async () => {
@@ -28,14 +36,26 @@ function SelectTeam() {
     };
     fetchData();
   }, [])
+
   return (
     <div className='select-team'>
+
       <div className='selectteam-content'>
+        <h1>Add Team Members</h1>
         <div className='initials-search'><input type="text" value={search} onChange={handleChange} spellcheck="false" /><Search size={35} color='#257adcff' /></div>
         <button className='all-members' onClick={showAllMembers}> View all</button>
         <div className='initials-team'>
           {team.map((x, index) => (
-            <p key={index}>Member {index + 1}: {x.firstname} {x.lastname}</p>
+            <div key={index} className="cartprod">
+              <p>{index + 1}: </p>
+              <p className='fname'>{x.firstname}</p>
+              <p className='lname'>{x.lastname}</p>
+              <p className='xemail'>{x.email}</p> 
+              <span className="remove">
+                <X />
+              </span>
+            
+            </div>
           ))}
         </div></div>
       {showModal && <div class="modal-content">
@@ -45,22 +65,27 @@ function SelectTeam() {
         </div>
         <div class="modal-body">
           <table>
-            <tr>
-              <th>Sr.no</th>
-              <th></th>
-              <th>Sr.no</th>
-            </tr>
-            {people.map((x, index) => (
-              // <p> {index + 1}: {x.firstname} {x.lastname}</p>
-
-              <tr key={index} className='modal-body-tr'>
-                <td>{index + 1}</td>
-                <td>{x.firstname}</td>
-                <td>{x.lastname}</td>
-                <td>{x.email}</td>
-                <td>{x.shift}</td>
+            <thead>
+              <tr>
+                <th>Sr.no</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Shift</th>
+                <th>Add</th>
               </tr>
-            ))}
+            </thead>
+            <tbody>
+              {people.map((x, index) => (
+                // <p> {index + 1}: {x.firstname} {x.lastname}</p>
+
+                <tr key={index} className='modal-body-tr'>
+                  <td>{index + 1}</td>
+                  <td>{x.firstname} {x.lastname}</td>
+                  <td>{x.email}</td>
+                  <td>{x.shift}</td>
+                  <td><button className="add-teammate" onClick={(e) => (handleAddMember(e, index))}><Plus /></button></td>
+                </tr>
+              ))}</tbody>
           </table>
         </div>
       </div>
